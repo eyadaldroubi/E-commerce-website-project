45d681eaad0d3ac368f1653149173c14d6b3c8fb
EYAD <eyadaldroubi@gmail.com>
2026-07-08 14:52:38 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+      base: '/E-commerce-website-project/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
